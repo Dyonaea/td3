490efad8e0a310c5fdb49f4c55bd231ad242a64a
cherrie235 <mathias.cherrier@gmail.com>
2025-10-15 14:54:25 +0200

    fin td3

--- src/server.js
+++ src/server.js
@@ -1,9 +1,36 @@
-import http from 'http';
+import express from 'express';
 
-const serv = http.createServer((req, res)=>{
-    res.end("I'm am not your enemy i am THE enemy")
-});
-const mouahaha = 3000;
-serv.listen(process.env.POST || 3000, ()=>{
-    console.log(`serveur running on http://localhost:${process.env.POST || mouahaha}`)
+const PORT = process.env.PORT || 3000;
+
+const app = express()
+
+app.use(express.json())
+
+app.get('/questions', (req, res) =>{
+    res.status(200).send([
+        {
+            id:'1',
+            question: 'Quelle est la capitale de la France',
+            answer: 'Paris'
+        }
+    ])
+})
+
+app.post('/questions', (req, res) =>{
+    const {question, answer} = req.body
+    if (!question || !answer){
+        return res.status(400).send({error:'errorrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr'})
+    }
+    res.status(201).send({
+        message:'question created successfully'
+    })
+})
+
+app.delete('/questions/:id', (req, res) =>{
+    const {id} = req.params
+    res.status(200).send({message: `Question ${id} deleted`})
+})
+
+app.listen(PORT, ()=>{
+    console.log(`serveur running on http://localhost:${PORT}`)
 })
